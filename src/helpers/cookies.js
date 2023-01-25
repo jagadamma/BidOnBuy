40ae8a8139a2051f8dc3f0fff2063f3c77b6ea1a
Nadeem Ahmad <nadeemgoogl786@gmail.com>
2023-01-25 11:58:07 +0530

    cookies.js update

--- src/helpers/cookies.js
+++ src/helpers/cookies.js
@@ -3,7 +3,8 @@ import Cookies from 'js-cookie'
 export const setCookie = (key, value) => {
   Cookies.set(key, value, {
     expires: 1,
-    sameSite: 'none'
+    sameSite: 'none',
+    secure: true
   })
 }
 
