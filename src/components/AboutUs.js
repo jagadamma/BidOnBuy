a48d64bc7a3649a5b289de1eecb9955ed3564dad
pawan-s19 <110162326+pawan-s19@users.noreply.github.com>
2023-01-27 14:37:54 +0530

    Create AboutUs.js

---
+++ src/components/AboutUs.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-center mt-5 ">
+      <div className="w-50 text-center">
+        <h1 className="mb-3">About Us</h1>
+        <p>
+          Bidonbuy was Founded in 2022, and we intend to create the biggest
+          platform for Buyers and Seller to connect to each other for successful
+          growth in the business. Also, we respect your privacy, Hence we will
+          be not sharing any personal details with buyers or sellers. You can
+          chat with Buyers or sellers for free of cost on our website.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
